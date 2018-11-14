730dc32f39e5f0a5a63c33eea8b6c936563806a8
Todd Matheson <tmatheson11186@gmail.com>
2018-11-13 16:36:10 -0800

    Trying new fonts, other style changes.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -28,8 +28,8 @@ class Layout extends React.Component {
     };
 
     if (typeof window !== `undefined`) {
-      this.loadFont("font400", "Source Sans Pro", 400);
-      this.loadFont("font600", "Montserrat", 700);
+      this.loadFont("font400", "Exo", 400);
+      this.loadFont("font600", "Vollkron", 700);
     }
   }
 
@@ -143,14 +143,14 @@ class Layout extends React.Component {
                       }
                       body {
                         font-family: ${this.state.font400loaded
-                          ? "Source Sans Pro, serif;"
+                          ? "Exo, serif;"
                           : "Arial, sans-serif;"};
                       }
                       h1,
                       h2,
                       h3 {
                         font-family: ${this.state.font600loaded
-                          ? "Montserrat, serif;"
+                          ? "Vollkron, serif;"
                           : "Arial, sans-serif;"};
                         font-weight: ${this.state.font600loaded ? 700 : 600};
                         line-height: 1.1;
