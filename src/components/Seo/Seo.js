a82b22f706278812cad8292ae639eb1b29adb7eb
Todd Matheson <tmatheson11186@gmail.com>
2018-11-14 22:48:32 -0800

    Complete rewrite SEO component :smile:

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -1,105 +1,50 @@
-import path from "path";
 import React from "react";
-import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
-import SchemaOrg from "./SchemaOrg";
+import Helmet from "react-helmet";
+import config from "../../../content/meta/config";
 
-const Seo = ({ postData, postImage, isBlogPost, facebook }) => (
-  <StaticQuery
-    query={graphql`
-      {
-        site {
-          siteMetadata {
-            title
-            description
-            canonicalUrl
-            image
-            author {
-              name
-            }
-            organization {
-              name
-              url
-              logo
-            }
-            social {
-              twitter
-              fbAppID
-            }
-          }
-        }
-      }
-    `}
-    render={({ site: { siteMetaData: seo } }) => {
-      const postMeta = postData.childMarkdownRemark.frontmatter || {};
+const Seo = props => {
+  const { data, facebook } = props;
+  const postTitle = ((data || {}).frontmatter || {}).title;
+  const postDescription = ((data || {}).frontmatter || {}).description;
+  const postCover = ((data || {}).frontmatter || {}).cover;
+  const postSlug = ((data || {}).fields || {}).slug;
 
-      const title = postMeta.title || seo.title;
-      const description = postMeta.description || postData.excerpt || seo.description;
-      const image = postImage ? `${seo.canonicalUrl}${postImage}` : seo.image;
-      const url = postMeta.slug
-        ? `${seo.canonicalUrl}${path.sep}${postMeta.slug}`
-        : seo.canonicalUrl;
-      const datePublished = isBlogPost ? postMeta.datePublished : false;
+  const title = postTitle ? `${postTitle} - ${config.shortSiteTitle}` : config.siteTitle;
+  const description = postDescription ? postDescription : config.siteDescription;
+  const image = postCover ? postCover : config.siteImage;
+  const url = config.siteUrl + config.pathPrefix + postSlug;
 
-      return (
-        <React.Fragment>
-          <Helmet>
-            {/* General tags */}
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <meta name="image" content={image} />
-
-            {/* OpenGraph tags */}
-            <meta property="og:url" content={url} />
-            {isBlogPost ? <meta property="og:type" content="article" /> : null}
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
-            <meta property="fb:app_id" content={seo.social.fbAppID} />
-
-            {/* Twitter Card tags */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:creator" content={seo.social.twitter} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
-          </Helmet>
-          <SchemaOrg
-            isBlogPost={isBlogPost}
-            url={url}
-            title={title}
-            image={image}
-            description={description}
-            datePublished={datePublished}
-            canonicalUrl={seo.canonicalUrl}
-            author={seo.author}
-            organization={seo.organization}
-            defaultTitle={seo.defaultTitle}
-          />
-        </React.Fragment>
-      );
-    }}
-  />
-);
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: config.siteLanguage,
+        prefix: "og: http://ogp.me/ns/website#"
+      }}
+    >
+      {/* General tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      {/* OpenGraph tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:type" content="website" />
+      <meta property="fb:app_id" content={facebook.appId} />
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:creator"
+        content={config.authorTwitterAccount ? config.authorTwitterAccount : ""}
+      />
+    </Helmet>
+  );
+};
 
 Seo.propTypes = {
   data: PropTypes.object,
-  facebook: PropTypes.object.isRequired,
-  isBlogPost: PropTypes.bool,
-  postData: PropTypes.shape({
-    childMarkdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.any,
-      excerpt: PropTypes.any
-    })
-  }),
-  postImage: PropTypes.string
-};
-
-Seo.defaultProps = {
-  isBlogPost: false,
-  postData: { childMarkdownRemark: {} },
-  postImage: null
+  facebook: PropTypes.object.isRequired
 };
 
 export default Seo;
